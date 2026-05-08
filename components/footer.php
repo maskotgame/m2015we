4939b67691518b4efe0997b15507aad898f4c4ed
VandGD <238834702+maskotgame@users.noreply.github.com>
2026-05-08 11:40:05 +0200

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,5 @@
 <div class="navbar-fixed-bottom container text-center bg-white">
     <p><strong>m2015we</strong>
-    <br>Established: "the year of our lord <i><del><?php echo date('Y') ?></del></i> 2015"
+    <br>Established: "the year of our lord <i><del>2026</del></i> 2015"
     <br>All copyrighted material belongs to their righful owners. <b><a href="/images/baller.gif" class="td-n link-no-td">we're ballin'</a></b></p>
 </div>
